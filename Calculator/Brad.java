ff4460d60d4445cd79cf630581593c0805df8c52
brad <bradboratto@gmail.com>
2019-11-05 15:03:13 -0500

    prompt() function
    python has this already but ok

---
+++ Calculator/Brad.java
@@ -0,0 +1,16 @@
+import Scanner;
+
+class Brad {
+	public static void main(String[] args) {
+		
+	}
+
+
+	// prompt() from better langs (ie python)
+	public static string Prompt(String msg) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println(msg);
+		String reply = scan.nextLine();
+		
+	}
+}
