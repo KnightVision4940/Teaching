bedb018e111f89784cf2dd7e39065611efa2d08f
Antonio <ant.iatonna@gmail.com>
2019-11-05 15:49:57 -0500

    tony

--- Calculator/Antonio.java
+++ Calculator/Antonio.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
 public class Antonio {
+    static int number1;
+    static int number2;
+    static String operator;
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("What is your first number?");
+        int number1 = in.nextInt();
+
+        System.out.println("What is your second number?");
+        int number2 = in.nextInt();
+
+        System.out.println("What is your operation?");
+        String operator = in.nextLine();
+
+    //static void Calculation() {
+        
+        if (operator.equals ("addition")) {
+            System.out.println(number1 + number2);
+        }
+        else if (operator.equals ("subtraction")) {
+            System.out.println(number1 - number2);
+        }
+        else if (operator.equals ("multiplication")) {
+            System.out.println(number1 * number2);
+        }
+        else if (operator.equals ("division")) {
+            System.out.println(number1 / number2);
+        }
+        else {
+            System.out.println("Invalid operation!");
+        }
+   // }
     }
 }
