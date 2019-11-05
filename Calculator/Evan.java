2019ba0203ae78cf76ca54d7a3ae45af8ca828c5
EvanFarquhar <yayanave@gmail.com>
2019-11-05 15:49:16 -0500

    evan

--- Calculator/Evan.java
+++ Calculator/Evan.java
@@ -1,1 +1,20 @@
-anything
+import java.util.Scanner;
+
+public class Evan{
+  static  Scanner in=new Scanner(System.in);
+public static void main(String[] args) {
+ 
+int add=5;
+System.out.println(add);
+
+String okay = in.nextLine();
+System.out.println(okay);
+// for(i=0;i<10;i++);{
+
+//     5
+// }
+
+
+
+
+}}
