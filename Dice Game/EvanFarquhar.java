56c97cb5d1e715f919e0985e252b32442827554f
EvanFarquhar <yayanave@gmail.com>
2019-11-18 15:26:20 -0500

    done

---
+++ Dice Game/EvanFarquhar.java
@@ -0,0 +1,20 @@
+/* How to add generate a random number:
+int min = 1
+int max = 30
+int random = (int)(Math.random() * max + min);
+
+This will generate a random number from 1 to 30
+*/
+
+import java.util.Random;
+import java.util.Scanner;
+ 
+public class Evan{
+ static Scanner in = new Scanner(System.in);
+ static Random randnum = new Random();
+ public static void main(String[] args) {
+    int dice = randnum.nextInt(6) +1;
+System.out.println(dice);
+
+ }
+ }
