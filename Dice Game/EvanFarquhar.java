5de138a96b09a7f53e51bddef09696fdf20b7ab4
EvanFarquhar <yayanave@gmail.com>
2019-11-25 16:01:05 -0500

    Changes

--- Dice Game/EvanFarquhar.java
+++ Dice Game/EvanFarquhar.java
@@ -12,19 +12,57 @@ import java.util.Scanner;
 public class EvanFarquhar{
  static Scanner in = new Scanner(System.in);
  static Random randnum = new Random();
+ static int money = 100;
  public static void main(String[] args) {
+
+   System.out.println("this is how this game works, so you start with $100 and the goal is to get more money!");
+   play();
+ }
+
+ public static void play() {
+
      System.out.println("guess your number (1-6)");
-    int wrong = in.nextInt();
+     int wrong = in.nextInt();
+    
  if (wrong > 6) {
     System.out.println("it has to be less than 6");
    wrong = in.nextInt();
  }
-else if (wrong <1){
-   
+if (wrong < 1){
+   System.out.println("it has to be more than 0 and not 0 and up to 6!");
+   wrong = in.nextInt();
+}
+System.out.println("how much do you want to bet on your number?");
+int bet = in.nextInt();
+ 
+if (bet > money){
+   System.out.println("sorry you don't have enough money, bet something under $" + money);
+   bet = in.nextInt();
 }
 
    int  dice = randnum.nextInt(6) +1;  
-System.out.println(dice);
+System.out.println("random number is: " + dice);
 
+if (dice == wrong){
+  int win = bet * 2;
+  System.out.println("You win: " + win);
+   int winner = money + win;
+  System.out.println("Now you have$" + winner);
+}
+
+else {
+   int win = bet * 2;
+   System.out.println("You lose, but you could have won: " + win);
+   int lose = money - bet;
+    System.out.println("Now you only have$" + lose);
+}
+
+if (money > 0){
+   play();
+}
+
+else{
+   System.out.print("game over!!!");
+}
  }
  }
