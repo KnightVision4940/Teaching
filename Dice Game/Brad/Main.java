c30afaa647c740ecc12facae5744719addc11b33
brad <bradboratto@gmail.com>
2019-11-25 15:59:28 -0500

    Auto stash before merge of "master" and "origin/master"

--- Dice Game/Brad/Main.java
+++ Dice Game/Brad/Main.java
@@ -1,36 +1,33 @@
 import java.util.Scanner;
 import java.util.Random;
 class Main {
+	static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
-		static Scanner scan = new Scanner(System.in);
-		int SCORE_TO_WIN = 1000; // <-- important thing
-		System.out.println(makeBet());
-	}
-	public static String[] getPlayers() {
-		int players = askInt("# of players?");
-		for(int i = 0; i < players; i++) {
-			System.out.println(i);
+	int score = 100;       // <--
+	int SCORE_MULTIPLIER = 10;  // <--- important things
+	int SCORE_TO_WIN = 1000;    // <-- 
+	while(0 < score && score < SCORE_TO_WIN) {
+	System.out.println("guess 1-6: ");
+			int guess = scan.nextInt();
+			System.out.println("$ to bet?"); // literal skinner box
+			int bet = scan.nextInt();
+			int random = new Random().nextInt(6) + 1;
+			if(score > 0) {
+				System.out.println("wow. you went bankrupt. you absolute buffoon. one hundred dollars were given to you at the start of this game. and what did you do with them? lost every single money. how could you? what stroke of dog luck could have led you towards this incredible pitfall? i can imagine it now. bet 5. lose 5. bet 5. lose 5. that word. \"lose\". it must torment you. does it not? nobody is truly \"weak\", are they? obviously not. when did you think it was okay? why didn't you quit while you were ahead?if p your definition of \"ahead\" is away from crippling debt, inherited only to lose. i don't think i'll ever truly understand.");
+			}
+			if(bet < 0) {
+				System.out.println("bet above zero you absolute luzr\nyou just lost $1 because fraud is bad and you should feel bad");
+				score -= 1;
+			}
+			else if(random == guess) {
+				System.out.println("correct!");
+				score += SCORE_MULTIPLIER * bet;
+			}
+			else {
+				System.out.println("you failed");
+				score -= bet;
+			}
+		System.out.println("you have " + score + " monies");
 		}
-	}
-	public static int makeBet() {
-		askInt("Guess 1-6: "); // literal skinner box
-		int bet = askInt("$ to bet?");
-		int random = new Random().nextInt(max) + 1;
-		if(random == 1) {
-			System.out.println("wow u did it");
-			return 10 * bet;
-		}
-		System.out.println("you failed");
-		return -bet;
-	
-	public static int askInt(String question) {
-		System.out.println(question);
-		int answer = scan.nextInt();
-		return answer;
-	}
-	public static String ask(String question) {
-		System.out.println(question);
-		String answer = scan.nextLine();
-		return answer;
 	}
 }
