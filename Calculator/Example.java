f695334579d92193c35ad8179a4d42cd62d80803
Gabriel <gabriel.i.rueda@gmail.com>
2019-11-27 15:48:10 -0500

    Example.java

---
+++ Calculator/Example.java
@@ -0,0 +1,14 @@
+public class Example{
+    public static void main(String[] args) {
+        intt i = 3
+        double d = 34.4;
+
+        System.ot.prinln(d);
+        if d == 23{
+            print "Hello World"
+        };
+
+        if(d = 34){
+            Strring s = "hello world;
+        }
+}
--- Example.java
+++ Example.java
@@ -1,5 +1,30 @@
 public class Example{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       int x = 12;
+       double var = 123.434;
+       String bar = "hello";
+       System.out.println("hello world");
+       System.out.println(var);
+       System.out.println(bar);
+       var = 14;
+       System.out.println(var);
+       if(x==12){ 
+           x=78;
+       }
+       if(x==12){
+            System.out.println("Yo");
+       }else if(x==23){
+           System.out.println("to");
+       }
+       System.out.println(perimeter(23,3));
+       world("Hello");
+       world("bye");
+       world("good");
+    }
+    static int perimeter(int l, int w){
+        return (2*l)+(2*w);
+    }
+    static void world(String i){
+        System.out.println(i);
     }
 }
